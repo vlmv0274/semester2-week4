681b8f1d4ec695f8ccd33a0759d31d3ddf726f3a
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-13 01:09:20 +0000

    Update makeString.c

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Mathew Carling
+Student ID: 201990775
 */
 
 #include <stdio.h>
@@ -9,9 +9,16 @@ Student ID:
 
 int main( int argc, char **argv ) {
 
-    // define appropriate data to hold your answer
-
-    // process the command-line data using appropriate string functions
+    // appropiate data to hold up to 10*4 chars + n-1 "-"s
+    char newword[64] = "";
+    // initilises to first arg
+    for (int i = 1; i < argc; i++) {
+        strcat(newword, argv[i]);
+        
+        if (i < argc - 1) {
+            strcat(newword, "-");
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
